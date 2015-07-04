ba91833d8059ba7da55c1ccc2f5333a3e2a27504
adivandhya <adivandhya@yahoo.co.in>
2015-07-04 08:31:48 +0530

    first commit to make the repo available on pypi
    first commit to make the repo available on pypi
    content not yet written

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'beautify',
+  packages = ['beautify'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Simplifies the process of formatting and printing beautiful and responsive output in terminals',
+  author = 'Adivandhya B R',
+  author_email = 'adivandhya@yahoo.co.in',
+  url = 'https://github.com/adivandhya/beautify', # use the URL to the github repo
+  download_url = 'https://github.com/adivandhya/beautify/tarball/0.1', # I'll explain this in a second
+  keywords = ['terminal', 'console', 'output','beautify','simplify','colorma'], # arbitrary keywords
+  classifiers = [],
+)
